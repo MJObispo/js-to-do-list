5c17616510526cad55c2402b09fb37f66b555810
MJ Obispo <mjobispo30@gmail.com>
2022-07-18 19:46:47 +0800

    js added new task container upon click of addbtn

--- app.js
+++ app.js
@@ -8,6 +8,25 @@ window.addEventListener("load", ()=>{
 
         if (!taskName) {
             alert("No task input...");
+        } else {
+            const newTaskContainer = document.createElement("div");
+            newTaskContainer.classList.add("task");
+
+            let newTaskName = document.createElement("h1");
+            newTaskName.classList.add("task-name");
+            newTaskName.textContent = taskName;
+
+            const newTaskButtonDiv = document.createElement("div");
+            newTaskButtonDiv.classList.add("action-buttons");
+
+            const newTaskEditButton = document.createElement("button");
+            newTaskEditButton.type = "button";
+            newTaskEditButton.className = "editBtn";
+            newTaskEditButton.innerText = "Edit task";
+
+            const newTaskDeleteButton = document.createElement("button");
+            newTaskDeleteButton.innerText = "Delete";
+            newTaskDeleteButton.type = "button";
         }
     });
 });
